630ecc3de8b5f291897be42cd77975c3a230b5d5
Lev Danneman <levdanneman@gmail.com>
2022-07-24 15:59:03 -0400

    upgraded to react 18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = hydrateRoot(
+  container,
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+// const root = createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
